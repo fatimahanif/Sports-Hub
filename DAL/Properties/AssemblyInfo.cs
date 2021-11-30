a21884b74c5591d84080578164d1f9cd8cf0542c
fatimahanif <fatimahanif303@gmail.com>
2021-11-30 19:59:33 +0500

    Added a few tables in database

--- DAL/Properties/AssemblyInfo.cs
+++ DAL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataAccessLayer")]
+[assembly: AssemblyTitle("DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataAccessLayer")]
+[assembly: AssemblyProduct("DAL")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8b68bd68-8792-4457-8d1b-f07f8433c0ca")]
+[assembly: Guid("93be232a-aa7e-4c61-8188-28a84d0c7452")]
 
 // Version information for an assembly consists of the following four values:
 //
